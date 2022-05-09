60c7bc1419323da2ff6075d32e495ab58d047ca3
censoretti <rodrigopaiotti96@gmail.com>
2022-05-09 13:19:37 -0300

    more functions, and new style

--- calulator/script.js
+++ calulator/script.js
@@ -30,8 +30,8 @@ class Calculator {
 
 	compute() {
 		let result
-		const prev = parseFloat(this.currentOperand)
-		const curr = parseFloat(this.previousOprand)
+		const curr = parseFloat(this.currentOperand)
+		const prev = parseFloat(this.previousOprand)
 		if (isNaN(prev) || isNaN(curr)) return
 		switch (this.operation) {
 			case '+':
@@ -45,6 +45,12 @@ class Calculator {
 				break
 			case '-':
 				result = prev - curr
+				break
+			case '%':
+				result = prev % curr
+				break
+			case '^':
+				result = prev ** curr
 				break
 			default:
 				return
@@ -81,28 +87,66 @@ class Calculator {
 			this.previousOperandTextElement.innerText = ''
 		}
 	}
+
+	changeSign() {
+		let placeholder = this.getDisplayNumber(this.currentOperand)
+		if(isNaN(placeholder)) return
+		this.currentOperand = placeholder * (-1)
+	}
+
+	squareCalc() {
+		if(this.currentOperand === '') return
+		if(this.previousOprand !== '') {
+			this.compute()
+			this.previousOperandTextElement.innerText = ''
+		}
+		this.currentOperand = Math.sqrt(this.currentOperand)
+		this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+	}
+
+	registerNumber() {
+		if(registredNumber === undefined) {
+			if(this.currentOperand === '') return
+			if(this.previousOprand !== '') this.compute()
+			registredNumber = this.currentOperand
+			this.currentOperand = ''
+			this.previousOprand = ''
+			registerButton.style.setProperty('background-color', 'rgb(0, 255, 0, .3)')
+		} else {
+			this.currentOperand = registredNumber
+			registredNumber = undefined
+		}
+	}
 }
 
 const numberButtons = document.querySelectorAll('[dataNumber]')
 const operationButtons = document.querySelectorAll('[dataOperation]')
+const hiddenButons = document.querySelectorAll('.hidden')
+
 const equalButton = document.querySelector('[dataEqual]')
 const deleteButton = document.querySelector('[dataDelete]')
 const clearButton = document.querySelector('[dataClear]')
 const previousOperandTextElement = document.querySelector('[dataPreviousOperand]')
 const currentOperandTextElement = document.querySelector('[dataCurrentOperand]')
+const changeSignButton = document.querySelector('[dataChangeSign]')
+const gearButton = document.querySelector('[dataGear]')
+const gridOptions = document.querySelector('[dataGridOptions]')
+const squareButton = document.querySelector('[dataSquare]')
+const registerButton = document.querySelector('[dataRegister]')
+const meButton = document.querySelector('[dataME]')
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
 	button.addEventListener('click', () => {
-		calculator.appendNumber(button.innerText)
+		calculator.appendNumber(button.value)
 		calculator.updateDisplay()
 	})
 })
 
 operationButtons.forEach(button => {
 	button.addEventListener('click', () => {
-		calculator.chooseOperation(button.innerText)
+		calculator.chooseOperation(button.value)
 		calculator.updateDisplay()
 	})
 })
@@ -120,4 +164,38 @@ clearButton.addEventListener('click', () => {
 deleteButton.addEventListener('click', () => {
 	calculator.delete()
 	calculator.updateDisplay()
+})
+
+changeSignButton.addEventListener('click', () => {
+	calculator.changeSign()
+	calculator.updateDisplay()
+})
+
+let gridChange = false
+gearButton.addEventListener('click', () => {
+	if(gridChange) {
+		gridOptions.style.setProperty('grid-template-columns', 'repeat(4, 100px)')
+		gridChange = false
+		hiddenButons.forEach(element => element.toggleAttribute("hidden"))
+		gearButton.innerText = '>'
+	} else {
+		gridOptions.style.setProperty('grid-template-columns', 'repeat(5, 100px)')
+		gridChange = true
+		hiddenButons.forEach(element => element.toggleAttribute("hidden"))
+		gearButton.innerText = '<'
+	}
+})
+
+squareButton.addEventListener('click', () => {
+	calculator.squareCalc()
+})
+
+let registredNumber
+registerButton.addEventListener('click', () => {
+	calculator.registerNumber()
+	calculator.updateDisplay()
+})
+
+meButton.addEventListener('click', () => {
+		alert('https://www.linkedin.com/in/rodrigopaiotti/')
 })
